eb572b6c9da7c19e1ba349f9eb425cae1d88ac4f
Carlos Gimenez Berral <carlos.gimenez@external.wolterskluwer.com>
2024-09-24 13:22:11 +0200

    environment token and get query to api

--- src/app/dashboard/users-list/users-list.component.ts
+++ src/app/dashboard/users-list/users-list.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-users-list',
   templateUrl: './users-list.component.html',
   styleUrls: ['./users-list.component.css']
 })
-export class UsersListComponent {
 
+export class UsersListComponent implements OnInit {
+  constructor(private userService: UserService) { }
+
+  ngOnInit(): void {
+    this.getUsers();
+  }
+
+  getUsers(): void {
+    this.userService.getUsers().subscribe(data => {
+      console.log(data)
+    })
+  }
 }
---
+++ src/app/services/user.service.ts
@@ -0,0 +1,20 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  url = 'https://gorest.co.in/public/v2/users';
+  token = environment.TOKEN;
+
+  constructor(private http: HttpClient) { }
+
+  getUsers(): Observable<any> {
+    return this.http.get<any>(`${this.url}?access-token=${this.token}`)
+  }
+
+}
